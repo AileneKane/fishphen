46382b3b9baf3f8f05be7e2ed4d57c890c0c1d2e
AileneKane <aettinger@fasmail.harvard.edu>
2019-03-14 16:12:30 -0700

    updating testdata

--- analyses/testdata.R
+++ analyses/testdata.R
@@ -1,7 +1,7 @@
 #Simulate orca data and vary number of observations to see if increasing effort yields shift in estimate of first, peak, or last observation day.
 wholeyear=FALSE#TRUE If you want the test data to be year-round (with peak prob in the summer)
                 #FALSE If you want the test data to be fall only (i.e. after day 273, like puget sound, with peak  
-biasearly=TRUE#TRUE if you want observations to be biased early in the season
+biasearly=FALSE#TRUE if you want observations to be biased early in the season
               #FALSE if you want observations to be unbiased
 
 if(wholeyear==TRUE){
@@ -52,7 +52,7 @@ meanobs<-mean(obs$days[obs$whaleobs==1])
 #Look at effect of increasing effort on estimate of first observation and estimate of last observation
 nreps<-10#
 
-if(wholeyear=TRUE){
+if(wholeyear==TRUE){
   lowobs<-90#mean whales days first 20 years: ps=13.5, uss=88
   highobs<-150#mean whales days last 20 years: ps=27.7, uss=146.5
 }
@@ -63,7 +63,9 @@ highobs<-30#mean whales days last 20 years: ps=27.7, uss=146.5
 numobs<-c(rep(lowobs,nreps),rep(highobs,nreps))
 
  
-bias=c(1.5,1.6,1.7,1.8,1.9,2)
+#bias=c(1.5,1.6,1.7,1.8,1.9,2)
+numresps<-20
+
 for(b in bias){
   df <- as.data.frame(matrix(nrow=length(numobs)*nreps,ncol=7))
   colnames(df)<-c("nobs","firstest","lastest","meanest","firsttrue", "lasttrue","meantrue")
@@ -104,7 +106,7 @@ for (i in 1:length(numobs)){
 quartz()
 par(mfrow=c(1,2))
 #First obs)
-boxplot(df$firstest~as.factor(df$nobs), xlab="Number of days observed", ylab="Estimate of first obs (doy)", main=paste("First obs",bias))
+boxplot(df$firstest~as.factor(df$nobs), xlab="Number of days observed", ylab="Estimate of first obs (doy)", main=paste("First obs"))
 t<-t.test(df$firstest~as.factor(df$nobs), conf.level=0.95)
 mtext(paste("Change=",-1*round(t$estimate[1]-t$estimate[2], digits=2),"(",-1*round(t$conf.int[1],digits=2),",",-1*round(t$conf.int[2],digits=2),")", sep=""),side=3,line=-3, adj=1)
 #Last obs
